0326a6eafe8eb03d9b7a751b108fc8b6d2e2bff8
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-10-27 13:02:22 +0200

    better output

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	// gives the number of actions to be taken in order to
 	// turn str1 into str2
 	// output: 2 (remove "0" from str1, remove "l" from str1)
-	fmt.Println(lev(str1, str2, len(str1), len(str2)))
+	fmt.Printf("The Levenshtain distance between %s and %s is %d\n", str1, str2, lev(str1, str2, len(str1), len(str2)))
 
 }
 

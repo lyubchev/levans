1cd5574b4b4cf9fbc4c77d115582901bcc773bb0
Lyubo Lyubchev <lyubo_2317@abv.bg>
2020-10-27 11:46:32 +0200

    implement min max functions that work with ints

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	n := min(3, 5)
+	m := max(4, 5)
+
+	fmt.Println(n)
+	fmt.Println(m)
+
+}
+
+func min(x, y int) int {
+	if x <= y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
 }
